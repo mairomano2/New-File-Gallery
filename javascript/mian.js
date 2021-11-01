e4efb3c3e98255c12962692b588d5fbdf4683128
Maia Romano <mairomano2@gmail.com>
2021-11-01 10:38:57 -0300

    actualizacion de la nueva muestra y agregado de artistas

--- javascript/mian.js
+++ javascript/mian.js
@@ -1,6 +1,6 @@
 // //MOSTRAR UN ARTISTA RANDOM
 const url = "https://new-file-gallery.com.ar/artistas/"
-const artistas = ["eva-ferran" , "kahlil-mendoza" , "irina-zaidz" , "angel-albisu" , "florencia-pena" , "daniel-olcay" , "yasna-orellana" , "pablo-benitez" , "fernanda-gonzalez" , "brayan-viera" , "andres-amaya" , "federico-andrade" , "melo" , "giuli-duarte" , "cata-gonsalez-rossi" , "rosario-amendolara" , "pablo-funes" , "tati-alvarez" , "anahi-juarez" , "pablo-delbene" , "belu-abdala", "vanessa-rojas" , "ariel-fernando-moro"];
+const artistas = ["fiorella-balbo", "vanessa-karin", "marianela-muniz",  "gaby-lobato", "tania-sagle", "cristobal-morales", "jimena-yengle", "vale-lozano", "eva-ferran" , "kahlil-mendoza" , "irina-zaidz" , "angel-albisu" , "florencia-pena" , "daniel-olcay" , "yasna-orellana" , "pablo-benitez" , "fernanda-gonzalez" , "brayan-viera" , "andres-amaya" , "federico-andrade" , "melo" , "giuli-duarte" , "cata-gonsalez-rossi" , "rosario-amendolara" , "pablo-funes" , "tati-alvarez" , "anahi-juarez" , "pablo-delbene" , "belu-abdala", "vanessa-rojas" , "ariel-fernando-moro"];
 const random = Math.floor(Math.random() * artistas.length);
 const recomendacion = artistas[random]; 
 let link = url + recomendacion + "/" + recomendacion + ".html";
