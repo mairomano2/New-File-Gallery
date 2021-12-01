4e289376a43411020012f28b2e4739c9780078bd
Maia Romano <mairomano2@gmail.com>
2021-12-01 11:32:31 -0300

    muestra diciembre

--- javascript/mian.js
+++ javascript/mian.js
@@ -1,12 +1,12 @@
 // //MOSTRAR UN ARTISTA RANDOM
 const url = "https://new-file-gallery.com.ar/artistas/"
-const artistas = ["fiorella-balbo", "vanessa-karin", "marianela-muniz",  "gaby-lobato", "tania-sagle", "cristobal-morales", "jimena-yengle", "vale-lozano", "eva-ferran" , "kahlil-mendoza" , "irina-zaidz" , "angel-albisu" , "florencia-pena" , "daniel-olcay" , "yasna-orellana" , "pablo-benitez" , "fernanda-gonzalez" , "brayan-viera" , "andres-amaya" , "federico-andrade" , "melo" , "giuli-duarte" , "cata-gonsalez-rossi" , "rosario-amendolara" , "pablo-funes" , "tati-alvarez" , "anahi-juarez" , "pablo-delbene" , "belu-abdala", "vanessa-rojas" , "ariel-fernando-moro"];
+const artistas = ["hiroko-miura-velarde", "david-de-la-obviedad", "lot", "nicole-sepliarsky", "pablo-puente", "sofia-montoya", "fiorella-balbo", "vanessa-karin", "marianela-muniz", "gaby-lobato", "tania-sagle", "cristobal-morales", "jimena-yengle", "vale-lozano", "eva-ferran", "kahlil-mendoza", "irina-zaidz", "angel-albisu", "florencia-pena", "daniel-olcay", "yasna-orellana", "pablo-benitez", "fernanda-gese", "brayan-viera", "andres-amaya", "federico-andrade", "melo", "giuli-duarte", "cata-gonsalez-rossi", "rosario-amendolara", "pablo-funes", "tati-alvarez", "anahi-juarez", "pablo-delbene", "belu-abdala", "vanessa-rojas", "ariel-fernando-moro"];
 const random = Math.floor(Math.random() * artistas.length);
-const recomendacion = artistas[random]; 
+const recomendacion = artistas[random];
 let link = url + recomendacion + "/" + recomendacion + ".html";
 
 document.querySelector("#recomendacion").addEventListener("click", mostrarArtista);
-function mostrarArtista(){
+function mostrarArtista() {
   window.location = link
 };
 
@@ -14,14 +14,14 @@ function mostrarArtista(){
 const categorias = document.querySelectorAll('.categorias');
 const todas = document.querySelectorAll('.todas');
 
-for(let i = 0; i < categorias.length; i++){
+for (let i = 0; i < categorias.length; i++) {
   categorias[i].addEventListener('click', filterPosts.bind(this, categorias[i]));
 }
 
-function filterPosts(item){
+function filterPosts(item) {
   changeActivePosition(item);
-  for(let i = 0; i < todas.length; i++){
-    if(todas[i].classList.contains(item.attributes.id.value)){
+  for (let i = 0; i < todas.length; i++) {
+    if (todas[i].classList.contains(item.attributes.id.value)) {
       todas[i].style.display = "block";
     } else {
       todas[i].style.display = "none";
@@ -29,8 +29,8 @@ function filterPosts(item){
   };
 };
 
-function changeActivePosition(activeItem){
-  for(let i = 0; i < categorias.length; i++){
+function changeActivePosition(activeItem) {
+  for (let i = 0; i < categorias.length; i++) {
     categorias[i].classList.remove('active');
   };
   activeItem.classList.add('active');
